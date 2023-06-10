1051bb918a05ce47169260f6b04f484097e38a85
timothybagas <63138796+timothybagas@users.noreply.github.com>
2023-06-10 12:17:48 +0700

    fix: use os.getcwd as a base_path for config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask
 import configparser
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 #================================================= OTHER CONFIG ==========================================================
 config = configparser.RawConfigParser()
-base_path = "" # Need to add absolute path
+base_path = os.getcwd()
 
-conf_file = base_path+'config.ini'
+conf_file = '{base_path}/config.ini'.format(base_path=base_path)
 config.read(conf_file)
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'e8be6c76f90f01893eedc58ee07c65fcc1e4339b1a854dc1edc9969402a84bc2'
+app.config['SECRET_KEY'] = config['APP']['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = config['SQL']['SQL_URI'] 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
